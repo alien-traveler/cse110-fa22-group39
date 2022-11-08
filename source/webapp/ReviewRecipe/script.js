94813327e210fe9878fe4781bd765a088fe00adb
JavaPersuader <liuchuning831@gmail.com>
2022-11-08 01:43:01 -0800

    update review recipe(#16)

---
+++ source/webapp/ReviewRecipe/script.js
@@ -0,0 +1,13 @@
+// const AddoneCheckBox = document.querySelector('#other');
+// const AddoneText = document.querySelector('#OtherValue');
+// AddoneText.style.visibility = 'hidden';
+// AddoneCheckBox.addEventListener('change' , (event) => {
+//     if (AddoneCheckBox.checked){
+//         AddoneText.style.visibility = 'visible';
+//         AddoneText.value = '';
+
+//     }
+//     else{
+//         AddoneText.style.visibility = 'hidden';
+//     }
+// });
