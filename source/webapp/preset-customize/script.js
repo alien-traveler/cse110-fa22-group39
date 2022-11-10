cf224dc4d5c10389450ba56706eb0d456ab68103
J4cky1ove <tihu@ucsd.edu>
2022-11-10 14:56:54 -0800

    Connect pages in create recipe button

---
+++ source/webapp/preset-customize/script.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', init);
+
+function init(){
+    let leftButtonEl = document.querySelectorAll("button")[0];
+    leftButtonEl.addEventListener('click', () => {
+        window.location = "../preset-list/preset-recipies.html";
+    })
+    let rightButtonEl = document.querySelectorAll("button")[1];
+    rightButtonEl.addEventListener('click', () => {
+        window.location = "../CustomizeRecipe/customize.html";
+    })
+}
