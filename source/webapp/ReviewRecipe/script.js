52a4548339dd87e5fdbeae73ea2e18e9151fb5f1
J4cky1ove <tihu@ucsd.edu>
2022-11-13 21:02:54 -0800

    review recipe

--- source/webapp/ReviewRecipe/script.js
+++ source/webapp/ReviewRecipe/script.js
@@ -1,13 +1,43 @@
-// const AddoneCheckBox = document.querySelector('#other');
-// const AddoneText = document.querySelector('#OtherValue');
-// AddoneText.style.visibility = 'hidden';
-// AddoneCheckBox.addEventListener('change' , (event) => {
-//     if (AddoneCheckBox.checked){
-//         AddoneText.style.visibility = 'visible';
-//         AddoneText.value = '';
+window.addEventListener('DOMContentLoaded', init);
 
-//     }
-//     else{
-//         AddoneText.style.visibility = 'hidden';
-//     }
-// });
+function init(){
+    let custome = JSON.parse(localStorage.getItem("custom"));
+    
+    const recipeName = document.getElementById('coffeeName');
+    const coffeeType = document.getElementById('coffeeName1');
+    const drinkName = document.getElementById('drinkTypeInput');
+    const drinkSize = document.getElementById('sizeTypeInput');
+    const addOn = document.querySelectorAll('#Topping');
+
+    drinkType = {1: "Cappuccinos", 2: "Latte", 3: "Espresso"};
+    size = {"1": "S", "2": "M", "3": "L"};
+
+    recipeName.value = custome["recipeName"];
+    coffeeType.value = custome["coffeeType"];
+    drinkName.value = drinkType[custome["drinkType"]];
+    drinkSize.value = size[custome["size"]];
+
+    const customeAdd = custome["addOns"];
+    for(let i=0; i<customeAdd.length; i++) {
+        addOn[customeAdd[i]].checked = true;
+    }
+    const addOnArr = [];
+    for(let i=0; i<addOn.length; i++) {
+        if(addOn[i].checked) {
+            addOnArr.push(i);
+        }
+    }
+    const buttonEl = document.querySelector("button");
+    buttonEl.addEventListener('click', (event) => {
+        //event.preventDefault();
+        const review = {
+            "recipeName" : recipeName.value,
+            "coffeeType" : coffeeType.value,
+            "drinkType" :  drinkName.value,
+            "size" : drinkSize.value,
+            "addOns" : addOnArr,
+            "available shop": []
+        };
+        localStorage.setItem('review', JSON.stringify(review));
+    })
+}
