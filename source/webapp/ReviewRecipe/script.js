52a4a0e468e2a44538777aa413cedfddd2631b80
J4cky1ove <77074167+J4cky1ove@users.noreply.github.com>
2022-11-16 00:31:49 -0800

    Jacky (#16)
    * Connect pages in create recipe button
    
    * html: bugfixes and minor adjustments; (#14)
    
    js: coffee customization from presets and scratch;
    recipes: template
    
    * Implement coffee customization (#15)
    
    * html: bugfixes and minor adjustments;
    js: coffee customization from presets and scratch;
    recipes: template
    
    * Implement coffee custommization
    
    * Implement coffee customization
    
    * fix bugs for customize
    
    * review recipe
    
    * implement shop searching
    
    Co-authored-by: Randi Li <114822529+RandiLi@users.noreply.github.com>
    Co-authored-by: Mingyi <mingyili09@aliyun.com>

--- source/webapp/ReviewRecipe/script.js
+++ source/webapp/ReviewRecipe/script.js
@@ -1,13 +1,61 @@
-// const AddoneCheckBox = document.querySelector('#other');
-// const AddoneText = document.querySelector('#OtherValue');
-// AddoneText.style.visibility = 'hidden';
-// AddoneCheckBox.addEventListener('change' , (event) => {
-//     if (AddoneCheckBox.checked){
-//         AddoneText.style.visibility = 'visible';
-//         AddoneText.value = '';
+window.addEventListener('DOMContentLoaded', init);
 
-//     }
-//     else{
-//         AddoneText.style.visibility = 'hidden';
-//     }
-// });
+function init(){
+    let custome = JSON.parse(localStorage.getItem("custom"));
+    
+    const recipeName = document.getElementById('coffeeName');
+    const coffeeType = document.getElementById('coffeeName1');
+    const drinkName = document.getElementById('drinkTypeInput');
+    const drinkSize = document.getElementById('sizeTypeInput');
+    const addOn = document.querySelectorAll('#Topping');
+    let shopEl;
+
+    let drinkType = {1: "Cappuccinos", 2: "Latte", 3: "Espresso"};
+    let size = {"1": "S", "2": "M", "3": "L"};
+
+    recipeName.value = custome["recipeName"];
+    coffeeType.value = custome["coffeeType"];
+    drinkName.value = drinkType[custome["drinkType"]];
+    drinkSize.value = size[custome["size"]];
+
+    const customeAdd = custome["addOns"];
+    for(let i=0; i<customeAdd.length; i++) {
+        addOn[customeAdd[i]].checked = true;
+    }
+    const addOnArr = [];
+    for(let i=0; i<addOn.length; i++) {
+        if(addOn[i].checked) {
+            addOnArr.push(i);
+        }
+    }
+
+    const review = {
+        "recipeName" : recipeName.value,
+        "coffeeType" : coffeeType.value,
+        "drinkType" :  drinkName.value,
+        "size" : drinkSize.value,
+        "addOns" : addOnArr,
+        "availableShop": []
+    };
+
+    //check shops
+    let shops = JSON.parse(localStorage.getItem("shops"));
+    let numShops = 1;
+    console.log(shops[0]["drinkType"]);
+    console.log(drinkName.value);
+    for (let i = 0; i < shops.length; i++){
+        if (shops[i]["coffeeType"].includes(coffeeType.value)
+        && shops[i]["drinkType"].includes(drinkName.value)
+        && shops[i]["size"].includes(drinkSize.value)){
+            review["availableShop"].push(shops[i]["shopName"]);
+            shopEl = document.getElementById(`shopName${numShops}`);
+            shopEl.value = shops[i]["shopName"];
+            numShops++;
+        }
+    }
+    console.log(review);
+    const buttonEl = document.querySelector("button");
+    buttonEl.addEventListener('click', () => {
+        localStorage.setItem('review', JSON.stringify(review));
+    })
+}
--- source/webapp/preset-customize/script.js
+++ source/webapp/preset-customize/script.js
@@ -2,7 +2,6 @@ window.addEventListener('DOMContentLoaded', init);
 window.onload = function(){
     this.loadHome();
 }
-
 function init(){
     let leftButtonEl = document.querySelectorAll("button")[0];
     leftButtonEl.addEventListener('click', () => {
@@ -10,6 +9,10 @@ function init(){
     })
     let rightButtonEl = document.querySelectorAll("button")[1];
     rightButtonEl.addEventListener('click', () => {
+        const index = localStorage.getItem('index');
+        if (index){
+            localStorage.removeItem('index');
+        }
         window.location = "../CustomizeRecipe/customize.html";
     })
 }
