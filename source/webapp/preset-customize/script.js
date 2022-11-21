ddf190d16fe5abdb02f5a988f66af99c8cf28f96
Will <jiwenwillluo@gmail.com>
2022-11-21 15:29:29 -0800

    include code for testing jsdoc

---
+++ source/webapp/preset-customize/script.js
@@ -0,0 +1,22 @@
+window.addEventListener('DOMContentLoaded', init);
+window.onload = function(){
+    this.loadHome();
+}
+function init(){
+    let leftButtonEl = document.querySelectorAll("button")[0];
+    leftButtonEl.addEventListener('click', () => {
+        window.location = "../preset-list/preset-recipies.html";
+    })
+    let rightButtonEl = document.querySelectorAll("button")[1];
+    rightButtonEl.addEventListener('click', () => {
+    const index = localStorage.getItem('index');
+    if (index){
+        localStorage.removeItem('index');
+    }
+    const savedIndex = localStorage.getItem('savedIndex');
+    if (savedIndex){
+        localStorage.removeItem('savedIndex');
+    }
+    window.location = "../CustomizeRecipe/customize.html";
+    })
+}
